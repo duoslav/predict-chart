ce6d740960314ce234afa0f2be175a8e30f29591
duoslav <vladislav.schepilo@gmail.com>
2023-01-29 18:33:24 +0100

    Added script that converts a csv file to csv with time windows (16 samples per line)

---
+++ test-generate-windowed-file.py
@@ -0,0 +1,36 @@
+import csv
+
+# Input file name
+input_file = 'sinusoidal_values.csv'
+# Output file name
+output_file = 'windows.csv'
+
+# Read data from input file
+with open(input_file, 'r') as f_in:
+    reader = csv.reader(f_in)
+    header = next(reader)
+    input_data = [list(map(float, row)) for row in reader]
+
+# Get number of rows and columns from input data
+num_rows = len(input_data)
+num_cols = len(input_data[0])
+
+# Create a list to store output data
+output_data = []
+
+window = 16
+
+# Iterate through each row
+for i in range(num_rows + 1 - window):
+    new_row = []
+
+    for col in range(num_cols):
+        for j in range(window):
+            new_row += input_data[i + j]
+
+    output_data.append(new_row)
+
+# Write data to output file
+with open(output_file, 'w') as f_out:
+    writer = csv.writer(f_out)
+    writer.writerows(output_data)
